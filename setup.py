e1c457b7367a66e6d72c10fb23aeee5aed8e732a
cosven <yinshaowen241@gmail.com>
2019-02-01 22:39:49 +0800

    init

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+
+setup(
+    name='fuo_xiami',
+    version='0.1.dev0',
+    description='feeluown xiami plugin',
+    author='Cosven',
+    author_email='yinshaowen241@gmail.com',
+    packages=[
+        'fuo_xiami',
+    ],
+    package_data={
+        '': []
+        },
+    url='https://github.com/feeluown/feeluown-xiami',
+    keywords=['feeluown', 'plugin', 'xiami'],
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+        ),
+    install_requires=[
+        'feeluown',
+    ],
+    entry_points={
+        'fuo.plugins_v1': [
+            'xiami = fuo_xiami',
+        ]
+    },
+)
